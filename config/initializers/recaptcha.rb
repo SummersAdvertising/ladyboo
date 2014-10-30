11d024bd26629389fbf86fc88079f39a34aabc23
Adam <adam@summers.com.tw>
2014-10-30 17:50:30 +0800

    fix lookbook layout

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,14 +1,14 @@
 Recaptcha.configure do |config|
 
-  if Rails.env.production? #Temporary: cama.summers.com.tw
+  if Rails.env.production? 
+    #Temporary: li750-66.members.linode.com
+    config.public_key  = '6Le04_wSAAAAAB9vNpb1AHUgI6E50Y6ojF6SCP9g'
+    config.private_key = '6Le04_wSAAAAAJHglo2Q4TR_lripR10Aw6YW2yiK'
 
-    config.public_key  = '6Lc_EPoSAAAAAKefE6nMepworHqRe0wtWdJtf7d8'
-    config.private_key = '6Lc_EPoSAAAAAKoNHBzl3XCWhtK2ZT3d_tzNn1El'
-    #config.proxy = 'http://myproxy.com.au:8080'
   else 
-    #Temporary: www.camacafe.com, because localhost is ok
-    config.public_key  = '6LcEsfgSAAAAAH0VkjdEFPXXc38FSMQ8zDu9YZ7b'
-    config.private_key = '6LcEsfgSAAAAALRIl7Zws6fm88A7dMfgbSLxQuve'
-    #config.proxy = 'http://myproxy.com.au:8080'
+    #Temporary: www.ladybootw.com, because localhost is ok
+    config.public_key  = '6Le14_wSAAAAAMrvjXFXDvn8EvoA_hBmA8WjBB9g'
+    config.private_key = '6Le14_wSAAAAADwR5NboXH_HAukOspkjnpkrTQYV'
+    
   end
 end
