8ced46e924901d8070b66fcc7a415a1df404a931
Hank <hank@summers.com.tw>
2014-10-31 18:39:17 +0800

    資料夾同步

---
+++ public/js/side.js
@@ -0,0 +1,25 @@
+$(function() {
+  var widthMove = $(window).width();
+  move();
+  
+  function move(){
+    if(widthMove<588){
+      topHeight1 = $('.main-content').height();
+      topHeight2 = $('.related').height();
+      topHeight3 = $('.sidebar').height();
+      $('#content').css({height:1400+topHeight1,overflow:'hidden'});
+      $('.sidebar').css({'margin-left':'-10px'})
+      $('#category-filter').css({position:'absolute',top:topHeight1+topHeight2+100,width:'100%'});
+      $('.related').css({position:'absolute',top:topHeight1+100,width:'96%','margin-left':'10px'});
+    }else{
+      $('#category-filter').css({position:'static'});
+      $('.related').css({position:'static'});
+      $('.sidebar').css({'margin-left':'0px'});
+      $('#content').css({height:'auto',overflow:'hidden'});
+    }
+  }
+  $(window).resize(function(){
+    widthMove = $(window).width();
+    move();
+  });
+});     
