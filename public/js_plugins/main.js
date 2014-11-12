b2af07cf7cf22609068a5de308a52084e97e7096
Adam <adam@summers.com.tw>
2014-11-12 15:53:37 +0800

    collect shared header/footer

---
+++ public/js_plugins/main.js
@@ -0,0 +1,26 @@
+  $(".pdquickview").click(function(){
+      pd_id = $(this).data("pd").toString();
+      $.ajax({
+      type: "GET",
+      url: "/products/"+pd_id+"/quickview",
+      data: { },
+      success: function(data){
+        // $('#thumb li a').removeClass('active'); 
+        // $('#thumb li a:first').addClass('active');
+        // $('.bxslider').css({'-webkit-transition': '0s', 'transition': '0s', '-webkit-transform': 'translate3d(0px, -380px, 0px)'}); 
+        
+        $(".inline").colorbox({
+          inline:true, 
+          width:"925",
+          height:"480",
+          href: "#inline_content",
+          open: true
+        });
+
+        return false   
+      },
+      error: function(data){
+        return false  
+      }
+    });
+  });
