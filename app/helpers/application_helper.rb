33d42c6a373c3d7fbf2ef11b37839f8164473162
Adam <adam@summers.com.tw>
2014-11-20 10:42:18 +0800

    test facebook meta

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,25 @@
 module ApplicationHelper
+
   def list_recently_viewed(viewed_array)
     if viewed_array && viewed_array.count > 0
       viewed_products = Product.includes(:category, :galleries).where(id: viewed_array)
       render partial: 'products/recently_viewed', locals: { products: viewed_products }
     end
   end
+
+  def aside_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, class: class_name) do
+      link_to link_text, link_path
+    end
+  end
+  # def controller?(*controller)
+  #   controller.include?(params[:controller])
+  # end
+
+  # def action?(*action)
+  #   action.include?(params[:action])
+  # end
+  
 end
