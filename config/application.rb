9d478819b0cd73e723cb423b35c59f379029d699
Adam <adam@summers.com.tw>
2014-12-29 19:06:17 +0800

    na

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,6 @@ module Ladyboo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-
-    
     
   end
 end
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -4,7 +4,9 @@ Recaptcha.configure do |config|
     #Temporary: li750-66.members.linode.com
     config.public_key  = '6Le04_wSAAAAAB9vNpb1AHUgI6E50Y6ojF6SCP9g'
     config.private_key = '6Le04_wSAAAAAJHglo2Q4TR_lripR10Aw6YW2yiK'
-
+    #Temporary: ladyboo.summers.com.tw
+    #config.public_key  = '6Lcfov8SAAAAAEQIqnDw_03A3cHxAn5BpBQrvW08'
+    #config.private_key = '6Lcfov8SAAAAAIEkbbgrEBMHl2kE-Sf1skpyfMee'
   else 
     #Temporary: www.ladybootw.com, because localhost is ok
     config.public_key  = '6Le14_wSAAAAAMrvjXFXDvn8EvoA_hBmA8WjBB9g'
